e2793440081e2f050445beff29fa24136e9475d8
gefen keinan <gefenk9@gmail.com>
2019-01-27 22:36:35 +0200

    found the tag that describes the chords

--- crawler/chordie_crawler.py
+++ crawler/chordie_crawler.py
@@ -2,21 +2,24 @@ import urllib.request
 from html.parser import HTMLParser
 
 class ChordieParser(HTMLParser):
-    def __init__(self, song_name, out_folder):
-        HTMLParser.__init__(self)
-        self.song_name = song_name
-
     def handle_data(self, data):
-        if "t:{}".format(self.song_name) in data.lower():
-            return (">>> ", data)
+        if "t:" in data.lower() and "st:" in data.lower():
+            lyrics, chords = parse_chords(data)
+            print(">>> ", chords)
+
+def parse_chords(data):
+    lines = data.split('\\n')
+    song_artist_name = "{}-{}".format(lines[0], lines[1])
+    print (lines)
+    chords = lines[2:]
+    return chords, chords
 
 
 opener = urllib.request.FancyURLopener({})
-url = "https://www.chordie.com/chord.pere/www.guitaretab.com/b/boyce-avenue/268379.html"
+url = "https://www.chordie.com/chord.pere/www.azchords.com/t/taylorswift-tabs-34955/shakeitoff2-tabs-877676.html"
 f = opener.open(url)
 content = f.read()
 
-parser = ChordieParser('perfect')
-l = parser.feed(str(content))
-print (l)
+parser = ChordieParser()
+parser.feed(str(content))
 
