156e28bd8b6ab4bae58c9f2134ae27ea61894a2d
gefen keinan <gefenk9@gmail.com>
2019-03-07 12:06:56 +0200

    added learning rnn epochs

--- model/create_training_data.py
+++ model/create_training_data.py
@@ -3,25 +3,26 @@ dir = r"C:\Users\gefenk\Documents\Deep Learning\chords_to_lyrics_generator\data\
 file = r"all-i-want-for-christmas-is-you"
 training_data = []
 
-with open(os.path.join(dir, file+".lyrics")) as f:
-    for index, line in enumerate(f.readlines()):
-        new_tup = ["", ""]
-        new_tup[0] = line
-        training_data.append(new_tup)
+def prep_data():
+    with open(os.path.join(dir, file+".lyrics")) as f:
+        for index, line in enumerate(f.readlines()):
+            new_tup = ["", ""]
+            new_tup[0] = line
+            training_data.append(new_tup)
 
-with open(os.path.join(dir, file + ".chords")) as f:
-    for index, line in enumerate(f.readlines()):
-        training_data[index][1] = line
+    with open(os.path.join(dir, file + ".chords")) as f:
+        for index, line in enumerate(f.readlines()):
+            training_data[index][1] = line
 
-word_to_ix = {}
-tag_to_ix = {}
+    word_to_ix = {}
+    tag_to_ix = {}
 
-for sent, tags in training_data:
-    for word in sent.split():
-        if word not in word_to_ix:
-            word_to_ix[word] = len(word_to_ix)
-    for tag in tags.split():
-        if tag not in tag_to_ix:
-            tag_to_ix[tag] = len(tag_to_ix)
+    for sent, tags in training_data:
+        for word in sent.split():
+            if word not in word_to_ix:
+                word_to_ix[word] = len(word_to_ix)
+        for tag in tags.split():
+            if tag not in tag_to_ix:
+                tag_to_ix[tag] = len(tag_to_ix)
 
-print(word_to_ix, tag_to_ix)
+    return word_to_ix, tag_to_ix
